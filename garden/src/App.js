986511d8552e13c6127db3668eb4533f2bc6886b
Valeria Matos <vmmprogramacion@gmail.com>
2023-02-14 21:06:31 -0300

    casi finalizado

--- garden/src/App.js
+++ garden/src/App.js
@@ -2,18 +2,20 @@ import React from 'react';  //para las dependencias de React. Es opcional.
 import NavBar from './components/NavBar';
 // import { boton } from './Button';
 // import { titulo } from './Titulo';
-import ItemListContainer from './components/ItemListContainer'
+import {ItemListContainer} from './components/ItemListContainer'
 
 const App = () =>
 {
-  const greeting = 'Poner algo acá'
-  return <NavBar>
-    {
+  const greeting = 'comprador'
+  return (
+    <>
+    <NavBar />
     <ItemListContainer greeting = {greeting}/>
-    /* <boton />
-    <titulo /> */}
-  </NavBar>;
-  /*Le paso los componentes como si fueran props a Navbar y este lo recibe como Children props*/
+    </>
+  )
+    
+  
+  
 };
 
 export default App;
---
+++ garden/src/components/ItemListContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const ItemListContainer = (props) => {
+    return (
+        <div>
+            <h2>
+                Bienvenido, {props.greeting}. 
+            </h2>
+        </div>
+    )
+}
